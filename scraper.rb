499b77e092e3702959cf04d9885a55580037169d
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2019-05-14 19:02:24 +0930

    Updated the URL and changed to extract the council reference. (#2)
    * Always save record as planningalerts now does versioning
    * Changed to parse applications on first page.
    * Corrected URL string construction.
    * Changed notice date selector.
    * Updated the address and notice date selectors.
    * Changed to parse multiple pages.
    * Improved the trace messages.
    * Changed to extract the application number.
    * Corrected the application number selector.
    * Changed to extract bolded date.
    * Trim the dot from the date and extract only the application number from the details page.
    * Changed to obtain all paragraph elements (not just the first).
    * Added date formatting.
    * Ensured that inner text is one line (so regular expression will work).
    * Added comments.
    * Changed the URL construction slightly.
    * Added parsing of the date using regular expressions.

--- scraper.rb
+++ scraper.rb
@@ -1,20 +1,63 @@
 require 'scraperwiki'
 require 'mechanize'
 
-url = "http://www.vincent.wa.gov.au/Your_Community/Whats_On/Community_Consultation/Planning_Applications"
+url = "https://imagine.vincent.wa.gov.au/planning-consultations"
 
 agent = Mechanize.new
-page = agent.get(url)
-page.search('.listitemrow').each do |i|
-  info_url = i.at('a')['href']
-  record = {
-    'council_reference' => info_url.split('/')[-2..-1].join('/'),
-    'address' => i.at('.address br').next_sibling.inner_text.gsub("\r\n", "").squeeze(' ').strip,
-    'description' => i.at('.listitemtext > strong').next_sibling.inner_text,
-    'info_url' => info_url,
-    'comment_url' => 'mailto:mail@vincent.wa.gov.au',
-    'date_scraped' => Date.today.to_s,
-    'on_notice_to' => i.at('.listitemtext > b').next_sibling.inner_text.split('/').reverse.join('-')
-  }
-  ScraperWiki.save_sqlite(['council_reference'], record)
+page_number = 0
+loop do
+  page_number += 1
+  page_url = "#{url}?page=#{page_number}"
+  page = agent.get(page_url)
+
+  puts "Parsing the results on page #{page_number}: #{page_url}"
+  application_count = 0
+  page.search('li.shared-content-block').each do |li|
+    application_count += 1
+    info_url = li.at('a')['href']
+    details_page = agent.get(info_url)
+    
+    # Extract all text after "Serial Number:" from the second paragraph under the div which has class "truncated-description".
+    council_reference = details_page.search('div.truncated-description p')[1].inner_text.gsub(/[\r\n]/, "").sub(/.*Serial Number:/, '').squeeze(' ').strip
+    
+    # Attempt to find a date.
+    # Extract all text from the first <b>...</b> element under the div which has class "truncated-description" (and trim the trailing ".").
+    possible_date_1 = li.search('div.truncated-description b')[0].inner_text.gsub(/[\r\n]/, "").squeeze(' ').strip.gsub(/\.$/, '')
+    # Extract all text from the second <b>...</b> element under the div which has class "truncated-description" (and trim the trailing ".").
+    possible_date_2 = li.search('div.truncated-description b')[1].inner_text.gsub(/[\r\n]/, "").squeeze(' ').strip.gsub(/\.$/, '')
+    matches_1 = possible_date_1.scan(/\b[0-9][0-9]?\s+[A-Z][A-Z][A-Z][A-Z]?\s+[0-9][0-9][0-9][0-9]$/i)
+    matches_2 = possible_date_2.scan(/\b[0-9][0-9]?\s+[A-Z][A-Z][A-Z][A-Z]?\s+[0-9][0-9][0-9][0-9]$/i)
+    parsed_date = ""
+    if (matches_1.length >= 1)
+      parsed_date = Date.parse(matches_1[0]).to_s
+    elsif (matches_2.length >= 1)
+      parsed_date = Date.parse(matches_2[0]).to_s
+    end
+    
+    record = {
+      'council_reference' => council_reference,
+      'address' => li.at('a').inner_text.gsub("\r\n", "").squeeze(' ').strip,
+      # Extract all text after "Development Details:" under the div which has class "truncated-description".
+      'description' => li.at('div.truncated-description').inner_text.gsub(/[\r\n]/, "").sub(/.*Development Details:/, '').squeeze(' ').strip,
+      'info_url' => info_url,
+      'comment_url' => 'mailto:mail@vincent.wa.gov.au',
+      'date_scraped' => Date.today.to_s,
+      'on_notice_to' => parsed_date
+    }
+      
+    puts "Saving page #{page_number} application record #{application_count}."
+    puts "    council_reference: " + record['council_reference']
+    puts "              address: " + record['address']
+    puts "          description: " + record['description']
+    puts "             info_url: " + record['info_url']
+    puts "          comment_url: " + record['comment_url']
+    puts "         date_scraped: " + record['date_scraped']
+    puts "         on_notice_to: " + record['on_notice_to']
+    ScraperWiki.save_sqlite(['council_reference'], record)
+  end
+  
+  # Continue paging until a page is encountered with no applications (or, as a safety precaution, a large number of pages have been processed).
+  puts "Found #{application_count} application(s) on page #{page_number}."
+  break if application_count == 0 or page_number > 100
 end
+puts "Complete."
